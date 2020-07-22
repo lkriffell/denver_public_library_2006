e92fc195b1865b802fbc4b4fd9e3ec8bfde8bd61
Logan Riffell <lkriffell@gmail.com>
2020-07-22 13:47:19 -0600

    Add publication_time_frame_for method and create_time_frame method

--- lib/library.rb
+++ lib/library.rb
@@ -15,4 +15,18 @@ class Library
     @books << author.books
     @books = @books.flatten
   end
+
+  def publication_time_frame_for(author)
+    book_years = author.books.map do |book|
+      book.publication_year
+    end
+    create_time_frame(book_years)
+  end
+
+  def create_time_frame(book_years)
+    time_frame = {}
+    time_frame[:start] = book_years.min
+    time_frame[:end] = book_years.max
+    time_frame
+  end
 end
