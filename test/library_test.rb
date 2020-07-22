ab7c83d4a0ef0a1083c81acf94d74ece8b51a32d
Logan Riffell <lkriffell@gmail.com>
2020-07-22 13:37:11 -0600

    add test_add_author for author class

--- test/library_test.rb
+++ test/library_test.rb
@@ -1,5 +1,6 @@
 require './lib/library'
 require './lib/author'
+require './lib/book'
 require "minitest/autorun"
 require "minitest/pride"
 
@@ -19,4 +20,25 @@ class LibraryTest < Minitest::Test
     assert_equal [], dpl.authors
   end
 
+  def test_add_author
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte",
+                                  last_name: "Bronte"})
+
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    harper_lee = Author.new({first_name: "Harper",
+                            last_name: "Lee"})
+
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    dpl.add_author_books(charlotte_bronte)
+    dpl.add_author_books(harper_lee)
+
+    assert_equal [charlotte_bronte, harper_lee], dpl.authors
+    assert_equal [jane_eyre, professor, villette, mockingbird], dpl.books
+  end
 end
