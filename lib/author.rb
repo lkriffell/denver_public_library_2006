6543608f5d4eb28918d161b25a5ced86f2f2f5ee
Logan Riffell <lkriffell@gmail.com>
2020-07-22 13:05:14 -0600

    Add write and add_book method

--- lib/author.rb
+++ lib/author.rb
@@ -8,4 +8,14 @@ class Author
     @name = "#{@first_name} #{@last_name}"
     @books = []
   end
+
+  def write(title, date)
+    book = Book.new({title: title, publication_date: date})
+    add_book(book)
+    book
+  end
+
+  def add_book(book)
+    @books << book
+  end
 end
