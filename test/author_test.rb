d3e1ef9760a7fd94f1d06194069b7bdcf54d058e
Logan Riffell <lkriffell@gmail.com>
2020-07-22 12:44:28 -0600

    Add test it exists for author

---
+++ test/author_test.rb
@@ -0,0 +1,20 @@
+require './lib/book'
+require './lib/author'
+require "minitest/autorun"
+require "minitest/pride"
+
+class AuthorTest < Minitest::Test
+
+  def setup
+    @charlotte_bronte = Author.new({first_name: "Charlotte",
+                                  last_name: "Bronte"})
+  end
+
+  def test_it_exists
+    assert_instance_of Author, @charlotte_bronte
+  end
+
+  #def test_it_has_attributes
+  #end
+
+end
