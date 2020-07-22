4947c57685156babd97f93c804b5df7e98a71469
Logan Riffell <lkriffell@gmail.com>
2020-07-22 14:05:19 -0600

    Add checkout method and attribute @checked_out_books

--- test/library_test.rb
+++ test/library_test.rb
@@ -35,8 +35,6 @@ class LibraryTest < Minitest::Test
     mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
     dpl.add_author(charlotte_bronte)
     dpl.add_author(harper_lee)
-    dpl.add_author_books(charlotte_bronte)
-    dpl.add_author_books(harper_lee)
 
     assert_equal [charlotte_bronte, harper_lee], dpl.authors
     assert_equal [jane_eyre, professor, villette, mockingbird], dpl.books
@@ -57,12 +55,39 @@ class LibraryTest < Minitest::Test
     mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
     dpl.add_author(charlotte_bronte)
     dpl.add_author(harper_lee)
-    dpl.add_author_books(charlotte_bronte)
-    dpl.add_author_books(harper_lee)
+
     charlotte_time_frame = {:start=>"1847", :end=>"1857"}
     harper_time_frame = {:start=>"1960", :end=>"1960"}
 
     assert_equal charlotte_time_frame, dpl.publication_time_frame_for(charlotte_bronte)
     assert_equal harper_time_frame, dpl.publication_time_frame_for(harper_lee)
+  end
+
+  def test_checkout
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte",
+                              last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    harper_lee = Author.new({first_name: "Harper",
+                            last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    assert_equal false, dpl.checkout(mockingbird)
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    assert_equal true , dpl.checkout(jane_eyre)
+
+    dpl.checkout(jane_eyre)
+
+    assert_equal [jane_eyre], dpl.checked_out_books
+  end
+
+  def test_return
+
   end
 end
