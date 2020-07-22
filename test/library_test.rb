bf97655f136b78e72a597513860e1602ec57ae9b
Logan Riffell <lkriffell@gmail.com>
2020-07-22 14:11:49 -0600

    Add test_return for return method

--- test/library_test.rb
+++ test/library_test.rb
@@ -88,6 +88,42 @@ class LibraryTest < Minitest::Test
   end
 
   def test_return
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte",
+                              last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
 
+    harper_lee = Author.new({first_name: "Harper",
+                            last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+
+    assert_equal [], dpl.checked_out_books
   end
+
+  # def test_most_popular_book
+  #   dpl = Library.new("Denver Public Library")
+  #   charlotte_bronte = Author.new({first_name: "Charlotte",
+  #                             last_name: "Bronte"})
+  #   jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+  #   professor = charlotte_bronte.write("The Professor", "1857")
+  #   villette = charlotte_bronte.write("Villette", "1853")
+  #
+  #   harper_lee = Author.new({first_name: "Harper",
+  #                           last_name: "Lee"})
+  #   mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+  #   dpl.add_author(charlotte_bronte)
+  #   dpl.add_author(harper_lee)
+  #   dpl.checkout(mockingbird)
+  #   dpl.return(mockingbird)
+  #   dpl.checkout(mockingbird)
+  #   dpl.return(mockingbird)
+  #   dpl.checkout(mockingbird)
+  #
+  # end
 end
