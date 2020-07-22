4e3783eb854f394e43d0d70e9247ef28b7f7c981
Logan Riffell <lkriffell@gmail.com>
2020-07-22 12:49:06 -0600

    Add attribute initialization for name and books

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,11 @@
 class Author
 
-  attr_reader :first_name, :last_name
+  attr_reader :first_name, :last_name, :name, :books
 
   def initialize(author)
     @first_name = author[:first_name]
     @last_name = author[:last_name]
+    @name = "#{@first_name} #{@last_name}"
+    @books = []
   end
 end
